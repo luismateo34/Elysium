e15862f494b88c1759b1363c75250c00219c0d76
luis <luismateo3461@gmail.com>
2025-09-17 06:13:59 -0300

    finalizada la pagina de contacto

---
+++ src/components/contact/menucontact.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import FlowingMenu from "@src/components/FlowingMenu";
+import mail from "@src/assets/mail.png";
+import Ig from "@src/assets/instagram.jpg";
+import github from "@src/assets/codigo.jpg";
+import linkedin from "@src/assets/linkedin.jpg";
+
+const githubUrl = import.meta.env.PUBLIC_GITHUB_URL;
+const email = import.meta.env.PUBLIC_EMAIL;
+const demoItems = [
+  {
+    link: "https://www.linkedin.com/in/luis-mateo-piñeiro-34arg/",
+    text: "Linkedin",
+    image: linkedin.src,
+  },
+  {
+    link: "https://www.instagram.com/elysiums_tech/",
+    text: "instagram",
+    image: Ig.src,
+  },
+  {
+    link: githubUrl,
+    text: "github",
+    image: github.src,
+  },
+  {
+    link: "#",
+    text: email,
+    image: mail.src,
+  },
+];
+
+export const MenuContact = () => {
+  return (
+    <div style={{ height: "600px", position: "relative" }}>
+      <FlowingMenu items={demoItems} />
+    </div>
+  );
+};
